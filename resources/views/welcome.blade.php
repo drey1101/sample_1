50e6eea6aa9a3a64531542c9d7ba8400684f67dd
drey1101 <163240931+drey1101@users.noreply.github.com>
2025-02-19 09:53:03 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,37 +16,37 @@ body {font-family: "Lato", sans-serif}
 
 <!-- Automatic Slideshow Images -->
 <div class="mySlides w3-display-container w3-center">
-  <img src="https://www.w3schools.com/w3images/la.jpg" style="width:100%">
+  <img src="https://your-vercel-project.vercel.app/w3images/la.jpg" style="width:100%">
 </div>
 <div class="mySlides w3-display-container w3-center">
-  <img src="https://www.w3schools.com/w3images/ny.jpg" style="width:100%">
+  <img src="https://your-vercel-project.vercel.app/w3images/ny.jpg" style="width:100%">
 </div>
 <div class="mySlides w3-display-container w3-center">
-  <img src="https://www.w3schools.com/w3images/chicago.jpg" style="width:100%">
+  <img src="https://your-vercel-project.vercel.app/w3images/chicago.jpg" style="width:100%">
 </div>
 
 <!-- Band Members -->
 <div class="w3-row w3-padding-32">
   <div class="w3-third">
-    <img src="https://www.w3schools.com/w3images/bandmember.jpg" class="w3-round" style="width:60%">
+    <img src="https://your-vercel-project.vercel.app/w3images/bandmember.jpg" class="w3-round" style="width:60%">
   </div>
   <div class="w3-third">
-    <img src="https://www.w3schools.com/w3images/bandmember.jpg" class="w3-round" style="width:60%">
+    <img src="https://your-vercel-project.vercel.app/w3images/bandmember.jpg" class="w3-round" style="width:60%">
   </div>
   <div class="w3-third">
-    <img src="https://www.w3schools.com/w3images/bandmember.jpg" class="w3-round" style="width:60%">
+    <img src="https://your-vercel-project.vercel.app/w3images/bandmember.jpg" class="w3-round" style="width:60%">
   </div>
 </div>
 
 <!-- Tour Locations -->
 <div class="w3-third w3-margin-bottom">
-  <img src="https://www.w3schools.com/w3images/newyork.jpg" style="width:100%" class="w3-hover-opacity">
+  <img src="https://your-vercel-project.vercel.app/w3images/newyork.jpg" style="width:100%" class="w3-hover-opacity">
 </div>
 <div class="w3-third w3-margin-bottom">
-  <img src="https://www.w3schools.com/w3images/paris.jpg" style="width:100%" class="w3-hover-opacity">
+  <img src="https://your-vercel-project.vercel.app/w3images/paris.jpg" style="width:100%" class="w3-hover-opacity">
 </div>
 <div class="w3-third w3-margin-bottom">
-  <img src="https://www.w3schools.com/w3images/sanfran.jpg" style="width:100%" class="w3-hover-opacity">
+  <img src="https://your-vercel-project.vercel.app/w3images/sanfran.jpg" style="width:100%" class="w3-hover-opacity">
 </div>
 
 <!-- Map Image -->
